8b8a79228c6cb9226fb87ad33fd5831ea197d70b
Mohlaroy <mohlaroy>
2025-08-09 08:18:08 +0500

    first commit

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+import { showAge, addNumbers, greet, fullName, rectanglePerimeter, triangleSides, squareNumber,
+    animalInfo, sayHello, showNames
+
+ } from "./script.js";
+
+//  showAge(5);
+
+//  addNumbers(3, 7);
+
+//  greet("Mohlaroy");
+
+//  fullName("Mohi", "Ahmedova");
+
+//  rectanglePerimeter(5, 3);
+
+//  triangleSides(3, 4, 5);
+
+//  squareNumber(5);
+
+//  animalInfo("Mushuk", 2);
+
+//  sayHello();
+
+showNames("Ali", "Vali", "Zilola", "Javohir", "Gulbahor");
---
+++ JS/script.js
@@ -0,0 +1,192 @@
+// 1 misol
+export function showAge(age) {
+  console.log(`Sizning yoshingiz: ${age}`);
+}
+// showAge(16);
+
+//export let showAge =  function(age) {
+//console.log(`Sizning yoshingiz: ${age}`);
+//};
+//  showAge(16);
+
+// let showAge = (age) => {
+//   console.log(`Sizning yoshingiz: ${age}`);
+// };
+//  showAge(16);
+
+// 2 misol
+export function addNumbers(a, b) {
+  console.log(a + b);
+}
+// addNumbers(3, 7);
+
+// let addNumbers = function(a, b){
+//     console.log(a + b)
+// }
+// addNumbers(3, 7);
+
+// let addNumbers = (a, b) => {
+//     console.log(a + b)
+// }
+// addNumbers(3, 7);
+
+// 3 misol
+export function greet(name) {
+  console.log(`Salom, ${name}!`);
+}
+//greet("Mohlaroy");
+
+// let greet = function(name){
+//     console.log(`Salom, ${name}!`)
+// }
+// greet("Mohlaroy");
+
+// let greet = (name) => {
+//     console.log(`Salom, ${name}!`)
+// }
+// greet("Mohlaroy");
+
+// 4 misol
+export function fullName(firstName, lastName) {
+  console.log(firstName, lastName);
+}
+// fullName('Mohi', 'Ahmadjonova');
+
+// let fullName = function(firstName, lastName){
+//     console.log(firstName, lastName)
+// }
+// fullName('Mohi', 'Ahmadjonova');
+
+// let fullName = (firstName, lastName) => {
+//     console.log(firstName, lastName)
+// }
+// fullName('Mohi', 'Ahmadjonova');
+
+// 5 misol
+export function rectanglePerimeter(length, width) {
+  console.log(2 * (length + width));
+}
+// rectanglePerimeter(5, 3)
+
+// let rectanglePerimeter =  function(length, width){
+//     console.log(2 * (length + width))
+// }
+// rectanglePerimeter(5, 3);
+
+// let rectanglePerimeter = (length, width) => {
+//     console.log(2 * (length + width));
+// }
+// rectanglePerimeter(5, 3);
+
+// 6 misol
+export function triangleSides(a, b, c) {
+  console.log(`Tomon a: ${a}`);
+  console.log(`Tomon b: ${b}`);
+  console.log(`Tomon c: ${c}`);
+}
+
+// triangleSides(3, 4, 5);
+
+// let triangleSides = function(a, b, c) {
+//     console.log(`Tomon a: ${a}`);
+//     console.log(`Tomon b: ${b}`);
+//     console.log(`Tomon c: ${c}`);
+// }
+
+// triangleSides(3, 4, 5);
+
+// let triangleSides = (a, b, c) => {
+//     console.log(`Tomon a: ${a}`);
+//     console.log(`Tomon b: ${b}`);
+//     console.log(`Tomon c: ${c}`);
+// }
+
+// triangleSides(3, 4, 5);
+
+// 7 misol
+export function squareNumber(num) {
+  console.log(num * num);
+}
+
+// squareNumber(5);
+
+// let squareNumber = function(num) {
+//     console.log(num * num);
+// }
+
+// squareNumber(5);
+
+// let squareNumber = (num) => {
+//     console.log(num * num);
+// }
+
+// squareNumber(5);
+
+// 8 misol
+export function animalInfo(name, age) {
+  console.log(`Bu ${name} ${age} yoshda.`);
+}
+
+// animalInfo("Mushuk", 2);
+
+// let animalInfo = function(name, age) {
+//     console.log(`Bu ${name} ${age} yoshda.`);
+// }
+
+// animalInfo("Mushuk", 2);
+
+// let animalInfo = (name, age) => {
+//     console.log(`Bu ${name} ${age} yoshda.`);
+// }
+
+// animalInfo("Mushuk", 2);
+
+// 9 misol
+export function sayHello() {
+  console.log("Salom, dunyo!");
+}
+
+// sayHello();
+
+// let sayHello2 = function() {
+//     console.log("Salom, dunyo!");
+// }
+
+// salomDunyo2();
+
+// let sayHello3 = () => {
+//     console.log("Salom, dunyo!");
+// }
+
+// sayHello3();
+
+// 10 misol
+export function showNames(name1, name2, name3, name4, name5) {
+  console.log(name1);
+  console.log(name2);
+  console.log(name3);
+  console.log(name4);
+  console.log(name5);
+}
+
+// showNames("Ali", "Vali", "Zilola", "Javohir", "Gulbahor");
+
+// let showNames = function(name1, name2, name3, name4, name5) {
+//     console.log(name1);
+//     console.log(name2);
+//     console.log(name3);
+//     console.log(name4);
+//     console.log(name5);
+// }
+
+// showNames("Ali", "Vali", "Zilola", "Javohir", "Gulbahor");
+
+// let showNames = (name1, name2, name3, name4, name5) => {
+//     console.log(name1);
+//     console.log(name2);
+//     console.log(name3);
+//     console.log(name4);
+//     console.log(name5);
+// }
+
+// showNames("Ali", "Vali", "Zilola", "Javohir", "Gulbahor");
